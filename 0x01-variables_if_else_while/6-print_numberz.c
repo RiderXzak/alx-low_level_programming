29aa0cf3dcf6a1f35acfba95855ddf74a991e6ec
RiderXzak <ziko.bensabar@gmail.com>
2023-07-16 09:44:17 -0700

    p

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int num;
+	int num;
 
 	for (num = 48; num < 58; num++)
-        {
-                putchar(num);
-        }
-        putchar('\n');
-        return (0);
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
 }
