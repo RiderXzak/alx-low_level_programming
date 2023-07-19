25196973a9534bf3f51a47390a62fa33daa9e618
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 13:33:00 -0700

    p

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ *@n: last digit to be printed
+ *Return: void
+ */
+
+int print_last_digit(int n)
+{
+	int L;
+
+	L = n % 10;
+
+	_putchar(L);
+	return (0);
+}
