d2dc283d8b5b7c7495e8273348799c6630d9b1cc
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 12:32:13 -0700

    p

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - transfrom the integer into abslote form
+ * @int: Integer need to be transformed
+ * Return: void
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		_putchar(-int);
+	}
+}
