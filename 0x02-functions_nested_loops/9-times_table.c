2256898e7a6448602f0eb2365831167c005e92d8
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 14:18:15 -0700

    p

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - Print time table
+ *
+ * Return:al
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+		int product = i * j;
+
+		_putchar((product / 10) + '0');
+		_putchar((product % 10) + '0');
+	}
+	}
+}
