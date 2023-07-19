04375fb848efbec670f04f85b65b1b32099466e9
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 15:58:39 -0700

    p

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Sum of two num
+ *@a: number
+ *@b: number
+ * Return:Al
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	_putchar(sum);
+	_putchar('\n');
+}
