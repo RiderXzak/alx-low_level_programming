736b02aee3332bd6fca35e15428453d6174d1545
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 15:48:04 -0700

    p

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,17 +11,17 @@ void times_table(void)
 	int j;
 	int k;
 	int l;
+	int p;
 
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-		int product = i * j;
-
-	if (product > 9)
+	p = i * j;
+	if (p > 9)
 	{
-	k = product % 10;
-	l = (k - product) / 10;
+	k = p % 10;
+	l = (p - k) / 10;
 	_putchar(',');
 	_putchar(' ');
 	_putchar(l + '0');
@@ -35,7 +35,7 @@ void times_table(void)
 			_putchar(' ');
 			_putchar(' ');
 		}
-		_putchar((product % 10) + '0');
+		_putchar((p % 10) + '0');
 	}
 	}
 	_putchar('\n');
