c41ce4210979a5aac0bb3886875afc092c3984f6
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 11:20:29 -0700

    p

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * return:Always 0
+ */
+
+int main(void)
+{
+	r = _islower('H');
+	_putchar(r + '0');	
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
