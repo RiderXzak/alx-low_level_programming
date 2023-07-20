0ab49d707aec35fdbd385fc80b49ceaa020ac933
RiderXzak <ziko.bensabar@gmail.com>
2023-07-20 11:14:03 -0700

    p

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,8 +12,10 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)  /*this part is causing infinite loops*/
+	/**
+	 * this part is causing infinite loops
+	 */
+	while (i < 10)
 	{
 		putchar(i);
 	}
