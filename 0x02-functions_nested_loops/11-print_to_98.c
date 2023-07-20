37c1f053160d8068dba6dd76c4d5ab99cc22a40f
RiderXzak <ziko.bensabar@gmail.com>
2023-07-19 17:15:01 -0700

    p

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,12 +15,12 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 	printf("%d", n);
-	_putchar('\n');
+	printf("\n");
 	break;
 	}
 	else
 	{
-	printf("%d", n);
+	printf("%d, ", n);
 	}
 	}
 	}
@@ -31,12 +31,12 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 			printf("%d", n);
-			_putchar('\n');
+			printf("\n");
 			break;
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d, ", n);
 			}
 		}
 	}
