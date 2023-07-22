436187c26fd308a59f2b71e3d5287331e9ef4ffe
RiderXzak <ziko.bensabar@gmail.com>
2023-07-21 19:04:52 -0700

    p

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,14 +4,13 @@
  * Return:al
  */
 
-
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -30,6 +29,7 @@ int main(void)
 	}
 
 	printf("\n");
+
 	return (0);
 }
 
