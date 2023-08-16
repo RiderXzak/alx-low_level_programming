7b3e378d98742a030b17f4e13b94add748c25d9d
RiderXzak <ziko.bensabar@gmail.com>
2023-08-16 15:47:33 -0700

    p

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers"
+/**
+ * int_index - indexing
+ * @array: array
+ * @size: size of array
+ * @cmp: function pointer to compare
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
