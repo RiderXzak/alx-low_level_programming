48a3ff22a3535751b8bdd29908b6d88c23a4b279
RiderXzak <ziko.bensabar@gmail.com>
2023-08-16 16:33:05 -0700

    p

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+ * op_add - add
+ * @a: int
+ * @b: int
+ * return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - add
+ * @a: int
+ * @b: int
+ * return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - add
+ * @a: int
+ * @b: int
+ * return: result
+ */
+int op_mul(int a, int b)
+{       
+	return (a * b);
+}   
+/**
+ * op_mod - add
+ * @a: int
+ * @b: int
+ * return: result
+ */
+op_mod(int a, int b)
+{       
+	return (a % b);
+}
+/**
+ * op_div - add
+ * @a: int
+ * @b: int
+ * return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
